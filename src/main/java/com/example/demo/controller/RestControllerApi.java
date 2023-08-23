b29ae11eae283051b843da68d8a7b19828ae5ecb
anisbhati <54569086+anisbhati@users.noreply.github.com>
2023-08-23 16:56:00 +0530

    dev commit

--- src/main/java/com/example/demo/controller/RestControllerApi.java
+++ src/main/java/com/example/demo/controller/RestControllerApi.java
@@ -6,33 +6,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.demo.repository.Customer;
-import com.sun.tools.javac.util.List;
+
 
 @RequestMapping
 public class RestControllerApi {
 
-	@Autowired
-	Customer crepo;
 
-	@GetMapping("/test")
-	public String getTest() {
-		return "betHeath up..........";
-	}
-
-
-	@GetMapping("/allcustomer")
-    public java.util.List<com.example.demo.entity.Customer>  getAllCostemer(){	
-	    //   java.util.List<com.example.demo.entity.Customer> l=crepo.findAll();
-	return null;
-
-	@GetMapping("/test2")
-	public String getTest2() {
-
-	@GetMapping("/test3")
-	public String getTest3() {
-
-		return "betHeath up..........";
-	}
-
-}
 }
