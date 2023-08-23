9788ecef2902bc0b144dd1ad406963b856d986ba
anisbhati <54569086+anisbhati@users.noreply.github.com>
2023-08-23 15:11:13 +0530

    customer entity add

---
+++ src/main/java/com/example/demo/entity/Customer.java
@@ -0,0 +1,46 @@
+package com.example.demo.entity;
+
+public class Customer {
+    int cid;
+    String name;
+    String mob;
+    String add;
+    String adhar;
+    String rid;
+	public int getCid() {
+		return cid;
+	}
+	public void setCid(int cid) {
+		this.cid = cid;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getMob() {
+		return mob;
+	}
+	public void setMob(String mob) {
+		this.mob = mob;
+	}
+	public String getAdd() {
+		return add;
+	}
+	public void setAdd(String add) {
+		this.add = add;
+	}
+	public String getAdhar() {
+		return adhar;
+	}
+	public void setAdhar(String adhar) {
+		this.adhar = adhar;
+	}
+	public String getRid() {
+		return rid;
+	}
+	public void setRid(String rid) {
+		this.rid = rid;
+	}
+}
