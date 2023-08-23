dc164f311fdd5d9e7b590b792f0cf9b2942b4f2c
anisbhati <54569086+anisbhati@users.noreply.github.com>
2023-08-23 16:06:11 +0530

    add api

--- src/main/java/com/example/demo/controller/RestControllerApi.java
+++ src/main/java/com/example/demo/controller/RestControllerApi.java
@@ -1,13 +1,27 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfigureOrder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.demo.repository.Customer;
+import com.sun.tools.javac.util.List;
+
 @RequestMapping
 public class RestControllerApi {
-    
+
+	@Autowired
+	Customer crepo;
+
 	@GetMapping("/test")
 	public String getTest() {
 		return "betHeath up..........";
 	}
+
+	@GetMapping("/allcustomer")
+    public java.util.List<com.example.demo.entity.Customer>  getAllCostemer(){	
+	    //   java.util.List<com.example.demo.entity.Customer> l=crepo.findAll();
+	return null;
+}
 }
